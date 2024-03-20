c76faa44b03f1033c325f3549485d2734a292a81
hyeeons <qouop0101@gmail.com>
2024-03-20 18:15:17 +0900

    헤더 만들기

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,7 @@ import { SlLock } from "react-icons/sl";
 import { LiaUserPlusSolid } from "react-icons/lia";
 import { LiaUser } from "react-icons/lia";
 import { SlEarphonesAlt } from "react-icons/sl";
+import { FaSearch } from "react-icons/fa";
 
 const MenuArr = [
   { icon: <SlLock />, text: "로그인" },
@@ -12,7 +13,7 @@ const MenuArr = [
 ];
 
 const Header: React.FC = () => (
-  <div className="flex flex-col justify-center h-[144px] gap-5">
+  <div className="flex flex-col justify-center h-[144px] gap-5 bg-[pink]">
     <div className="flex items-center justify-between ">
       <h1>
         <a href="#">MVSTORY</a>
@@ -26,14 +27,29 @@ const Header: React.FC = () => (
         ))}
       </div>
     </div>
-    <ul className="flex gap-5">
-      <li>
-        <a href="#">현재상영작</a>
-      </li>
-      <li>
-        <a href="#">상영예정작</a>
-      </li>
-    </ul>
+    <div className="flex justify-between px-[5px]">
+      <ul className="flex gap-5">
+        <li>
+          <a href="#">현재상영작</a>
+        </li>
+        <li>
+          <a href="#">상영예정작</a>
+        </li>
+      </ul>
+      <div className="relative w-[26%]">
+        <form action="/search" method="GET">
+          <input
+            type="text"
+            placeholder="영화제목을 입력해보세요"
+            className="w-full h-[37px] indent-[10px] focus:outline-none"
+          />
+          <button type="submit" className="absolute top-[25%] right-[7px]">
+            <FaSearch />
+            <p className="indent-[-9999px]">검색</p>
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
 );
 
